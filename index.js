97f89678af01782966c4855caa688fc9f35f913e
Gabriel Caycho <gabrielcaychojose8@hotmail.com>
2022-10-23 15:58:14 -0500

    agregando ultimas cosas al dom

--- index.js
+++ index.js
@@ -251,6 +251,7 @@ function validarCarrito(carritoObj) {
       <div class="carrito-item-descripcion">
         <span>Items</span>
         <span>Cantidad</span>
+              
       </div>
       <span onclick="eliminarItem(idProducto)" class="eliminarItem">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-x"
